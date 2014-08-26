9ec137b5d0176309c8db580afdaf6d787b99cd0c
Vivek Mangla <vivekFuneesh@users.noreply.github.com>
2014-08-26 21:42:02 +0530

    Create AboutPanel.java

---
+++ AboutPanel.java
@@ -0,0 +1,52 @@
+
+/**********************
+  *************************
+        @author::Vivek Mangla
+**************************
+*****************************/
+
+/*******************************************************************
+
+AboutPanel.java is part of Dimaagi_Keeda.
+
+    Dimaagi_Keeda is free Game based on 2048(an Android Game)BUT WITH LOT OF DIFFERENT FEATURES: you can redistribute it    and/or modify
+    it under the terms of the GNU General Public License as published     by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Dimaagi_Keeda is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Dimaagi_Keeda.  If not, see <http://www.gnu.org/licenses/>.
+
+*******************************************************************
+*/
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author vivek
+ */
+public class AboutPanel extends JPanel {
+    
+   
+   public AboutPanel(){
+    
+       
+       
+   }
+    
+    
+    @Override
+    public void paintComponent(Graphics g){super.paintComponent(g);
+    
+    
+   
+    }
+    
+}
